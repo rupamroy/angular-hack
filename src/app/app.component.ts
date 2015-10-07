f137eb84853fd85ef85e1d272aac3f98a19d1d20
Avinash Bhosale <avinash.bhosale@accenture.com>
2015-10-06 20:58:30 -0400

    Added Dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import {View, Component} from 'angular2/angular2';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {DashboardComponent} from './dashboard.component';
 
 @Component({ selector: 'my-app' })
 @View({
   template: `
-  <H1> HELLO WORLD </H1>
+  <router-outlet></router-outlet>
     `,
   directives: [ROUTER_DIRECTIVES]
 })
-
+@RouteConfig([
+  { path: '/', as: 'Dashboard', component: DashboardComponent }
+])
 export class AppComponent { }
---
+++ src/app/dashboard.component.ts
@@ -0,0 +1,23 @@
+import {Component, FORM_DIRECTIVES, View} from 'angular2/angular2';
+
+@Component({ selector: 'my-dashboard' })
+@View({
+	template: `
+		<h2>Dashboard</h2>
+		<div>Hello {{name}}</div>
+		<input [(ng-model)]="name" />
+        <input [(ng-model)]="emailAddress" />
+		<button (click)="addAttendee()">Add Attendee</button>
+		<p>{{message}}</p>
+	`,
+	directives: [FORM_DIRECTIVES]
+})
+export class DashboardComponent {
+	public name = 'bar';
+	public emailAddress = 'bar@foo.com';
+    public message = '';
+
+	addAttendee() {
+        this.message = this.name + ' ' + this.emailAddress;
+	}
+}
